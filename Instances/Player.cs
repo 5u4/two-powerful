eaa650709720ddc87cc90fd796cb04b7d82eee3a
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-06-26 19:23:14 -0700

    Add player animation

---
+++ Instances/Player.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public class Player : Node2D
+{
+
+}
---
+++ Instances/PlayerAnimatedSprite.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public class PlayerAnimatedSprite : AnimatedSprite
+{
+    public override void _Ready()
+    {
+        Play();
+    }
+}
