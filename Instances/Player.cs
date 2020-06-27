86d5e70a280a27aeb6ac899484bdd8f1dfc9ad8a
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-06-26 21:26:14 -0700

    Add tileset + player movement

--- Instances/Player.cs
+++ Instances/Player.cs
@@ -1,7 +1,38 @@
 using Godot;
 using System;
 
-public class Player : Node2D
+public class Player : KinematicBody2D
 {
+    private const float Speed = 70;
+    private const float Gravity = 300;
+    private const float Acceleration = 0.25f;
+    private const float Friction = 0.1f;
+    private const float LandingSpeedReduction = 0.25f;
+    
+    public Vector2 Velocity = Vector2.Zero;
 
+    public override void _PhysicsProcess(float delta)
+    {
+        GetHorizontalInput();
+        ApplyGravity(delta);
+        HandleMovement();
+    }
+
+    private void GetHorizontalInput()
+    {
+        var direction = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        Velocity.x = Math.Abs(direction) > 0
+            ? Mathf.Lerp(Velocity.x, direction * Speed, Acceleration)
+            : Mathf.Lerp(Velocity.x, 0, Friction);
+    }
+
+    private void ApplyGravity(float delta)
+    {
+        Velocity.y += (Velocity.y < 0 ? Gravity : Gravity * (1 - LandingSpeedReduction)) * delta;
+    }
+
+    private void HandleMovement()
+    {
+        Velocity = MoveAndSlide(Velocity, Vector2.Up);
+    }
 }
--- Instances/PlayerAnimatedSprite.cs
+++ Instances/PlayerAnimatedSprite.cs
@@ -3,8 +3,28 @@ using System;
 
 public class PlayerAnimatedSprite : AnimatedSprite
 {
+    private Player _player;
+    private const float RunningThreshold = 10f;
+    
     public override void _Ready()
     {
-        Play();
+        _player = GetNode<Player>("..");
+    }
+
+    public override void _Process(float delta)
+    {
+        HandleHorizontalFlip();
+        Play(GetAnimationState());
+    }
+
+    private void HandleHorizontalFlip()
+    {
+        int facing = Math.Sign(_player.Velocity.x);
+        if (facing != 0) FlipH = facing != 1;
+    }
+
+    private string GetAnimationState()
+    {
+        return Math.Abs(_player.Velocity.x) > RunningThreshold ? "run" : "idle";
     }
 }
