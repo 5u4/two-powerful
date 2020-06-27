4ba6f3ba42cb045231883c9ffc92bdadb8c2086d
Sen Hung Wong <alexwongsenhung@gmail.com>
2020-06-27 11:05:59 -0700

    Add player born

--- Instances/PlayerAnimatedSprite.cs
+++ Instances/PlayerAnimatedSprite.cs
@@ -7,6 +7,8 @@ public class PlayerAnimatedSprite : AnimatedSprite
     private PlayerJump _playerJump;
     private PlayerDash _playerDash;
     private PlayerAttack _playerAttack;
+    private bool _borning = true;
+    
     private const float RunningThreshold = 10f;
 
     public override void _Ready()
@@ -31,10 +33,22 @@ public class PlayerAnimatedSprite : AnimatedSprite
 
     private string GetAnimationState()
     {
+        if (_borning)
+        {
+            _player.ActionLockTimer.Lock(float.PositiveInfinity);
+            return "born";
+        }
         if (_playerAttack.IsAttacking) return "attack";
         if (_playerDash.IsDashing) return "dash";
         if (_player.IsOnFloor()) return Math.Abs(_player.Velocity.x) > RunningThreshold ? "run" : "idle";
         if (_player.Velocity.y < 0) return _playerJump.IsDoubleJumping() ? "doublejump" : "jump";
         return _player.Velocity.y > 0 ? "fall" : null;
+    }
+
+    private void _on_Born_animation_finished()
+    {
+        if (Animation != "born") return;
+        _borning = false;
+        _player.ActionLockTimer.Unlock();
     }
 }
